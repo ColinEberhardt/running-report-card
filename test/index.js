5b021601bada8dbcc6ce1e06d53f8ed5c5833925
Colin Eberhardt <colin.eberhardt@gmail.com>
2024-12-24 14:23:34 +0000

    other minor tweaks

--- test/index.js
+++ test/index.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 const workerFarm = require("worker-farm");
 const workers = workerFarm({ maxConcurrentWorkers: 1 }, require.resolve("./generate"));
 
+// load the contents of the ../serverless.env.yml YML file into the env variables
+const yaml = require("js-yaml");
+const path = require("path");
+const envFile = path.resolve(__dirname, "../serverless.env.yml");
+const env = yaml.load(fs.readFileSync(envFile, "utf8"));
+Object.keys(env).forEach(key => {
+  process.env[key] = env[key];
+});
+
 let queue = 0;
 fs.readdir("./static", (_, files) => {
   files.forEach(file => {
